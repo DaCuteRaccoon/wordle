e8124b55683344584d05afb382ca985441126410
DaCuteRaccoon stands with Ukraine <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-03-14 15:01:24 -0700

    Add definitions and includes

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <strings.h>
+#include <unistd.h>
+#include <cctype>
+#include <stdio.h>
+#include <stdlib.h>
 
+// Define colors
+
+#define RESET "\x1B[0m";
+#define BOLD "\x1B[1m";
+#define RED "\x1B[41m";
+#define GREEN "\x1B[42m";
+#define YELLOW "\x1B[103m";
+#define BLUE "\x1B[44m";
+#define MAGENTA "\x1B[105m";
+#define GREY "\x1B[100m";
+#define MINT "\x1B[102m";
+#define SKY "\x1B[104m";
+#define PEACH "\x1B[101m";
+#define CYAN "\x1B[46m";
